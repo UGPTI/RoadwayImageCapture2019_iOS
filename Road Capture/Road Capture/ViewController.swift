944e91f5d57376ad75ed806ebc16a8bff891d4fa
asletten <34174212+asletten@users.noreply.github.com>
2019-01-29 14:20:05 -0600

    Initial Project

---
+++ Road Capture/Road Capture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Road Capture
+//
+//  Created by Aaron Sletten on 1/29/19.
+//  Copyright © 2019 Aaron Sletten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Road Capture/Road CaptureTests/Road_CaptureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Road_CaptureTests.swift
+//  Road CaptureTests
+//
+//  Created by Aaron Sletten on 1/29/19.
+//  Copyright © 2019 Aaron Sletten. All rights reserved.
+//
+
+import XCTest
+@testable import Road_Capture
+
+class Road_CaptureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

07478a51511192956e83bf4e78d3a1fbe2c5cdb5
asletten <34174212+asletten@users.noreply.github.com>
2019-01-29 17:21:09 -0600

    Location Tests

--- Road Capture/Road Capture/ViewController.swift
+++ Road Capture/Road Capture/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+//        https://stackoverflow.com/questions/28952747/calculate-total-traveled-distance-ios-swift
+        
+        //Set up manager
+        let manager : CLLocationManager = CLLocationManager();
+        manager.delegate = self;
+        manager.requestWhenInUseAuthorization();
+        manager.activityType = CLActivityType.other;
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestLocation();
+        //manager.startUpdatingLocation();
+        
+        //Print location
+        print(manager.location?.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            print(location.coordinate);
+        }
     }
 
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Caught Error: \(error.localizedDescription)");
+    }
+        
 }
 

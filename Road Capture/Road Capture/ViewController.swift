f6fadff70a997dd1a8ced0d299330795c5994965
Aaron Sletten <aaron.j.sletten@ndus.edu>
2019-01-31 11:07:05 -0600

    Added Comments

--- Road Capture/Road Capture/ViewController.swift
+++ Road Capture/Road Capture/ViewController.swift
@@ -13,26 +13,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
-//        https://stackoverflow.com/questions/28952747/calculate-total-traveled-distance-ios-swift
+
+        //Link for finding the distance traveled - Not implimented
+        //https://stackoverflow.com/questions/28952747/calculate-total-traveled-distance-ios-swift
         
         //Set up manager
         let manager : CLLocationManager = CLLocationManager();
         manager.delegate = self;
+        
+        //Request Access to location services
         manager.requestWhenInUseAuthorization();
+        
+        //Set up activity and accuracy
         manager.activityType = CLActivityType.other;
         manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //Request location
         manager.requestLocation();
-        //manager.startUpdatingLocation();
         
         //Print location
         print(manager.location?.coordinate)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Print all locations
         for location in locations {
             print(location.coordinate);
         }

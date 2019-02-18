2b8cc96cff0aed7630dee64acbd92bc18845b287
Kelvin Boatey <boatey.kelvin23@gmail.com>
2019-02-18 14:26:51 -0600

    Basic Layout of the App

---
+++ Road Capture/Road Capture/ViewController/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Road Capture
+//
+//  Created by Kelvin Boatey on 2/18/19.
+//  Copyright © 2019 Aaron Sletten. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

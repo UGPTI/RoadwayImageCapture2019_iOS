4f3791d5c81429351c4f94a0a8a0edabb5aa2967
Kelvin Boatey <boatey.kelvin23@gmail.com>
2019-02-18 16:55:38 -0600

    Settings tableview cells configured

--- Road Capture/Road Capture/ViewController/SettingsTableViewController.swift
+++ Road Capture/Road Capture/ViewController/SettingsTableViewController.swift
@@ -10,8 +10,45 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
 
+    @IBOutlet weak var resolutionTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    
+    let resolution = ["High",
+                      "Medium",
+                      "Low"
+    ]
+    var selectedResolution: String?
+    var selectedDistance: Int?
+    
+    let distance = [100,
+                    200,
+                    300,
+                    400,
+                    500,
+                    600,
+                    700,
+                    800,
+                    900,
+                    1000
+    ]
+    
+    func createResolutionPicker() {
+        let resolutionPicker = UIPickerView()
+        resolutionPicker.delegate = self
+        
+        resolutionTextField.inputView = resolutionPicker
+    }
+    
+    func createDistancePicker() {
+        let distancePicker = UIPickerView()
+        distancePicker.delegate = self
+        
+        distanceTextField.inputView = distancePicker
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createResolutionPicker()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -32,59 +69,25 @@ class SettingsTableViewController: UITableViewController {
         return 0
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
-        return cell
+}
+extension SettingsTableViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return resolution.count
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return resolution[row]
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedResolution = resolution[row]
+        resolutionTextField.text = selectedResolution
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

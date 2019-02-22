366dc3e0cbe96b9ce5d90e2b7891eff16cbc7b76
Kelvin Boatey <boatey.kelvin23@gmail.com>
2019-02-21 21:30:45 -0600

    Camera Phase 1

--- Road Capture/Road Capture/ViewController/CameraViewController.swift
+++ Road Capture/Road Capture/ViewController/CameraViewController.swift
@@ -7,13 +7,85 @@
 //
 
 import UIKit
+import CoreLocation
+import AVFoundation
+
 
 class CameraViewController: UIViewController {
-
+    
+    //Set Up Camera Session
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    @IBAction func startButton_TouchUpInside(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet var cameraView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setudCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+        
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setudCaptureSession() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices{
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera =  device
+            } else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        
+        currentCamera = backCamera
+    }
+    
+    func setupInputOutput() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        } catch {
+            print(error)
+        }
+        
+        
+    }
+    
+    func setupPreviewLayer() {
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+    }
+    
+    func startRunningCaptureSession() {
+        captureSession.startRunning()
     }
     
 
@@ -26,5 +98,7 @@ class CameraViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+
 
 }

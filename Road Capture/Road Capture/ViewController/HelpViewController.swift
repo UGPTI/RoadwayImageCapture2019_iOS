1501c3d5b1422956d355f835cc89afdb2ea65f88
asletten <34174212+asletten@users.noreply.github.com>
2019-04-07 16:08:33 -0500

    Help page stuff

--- Road Capture/Road Capture/ViewController/HelpViewController.swift
+++ Road Capture/Road Capture/ViewController/HelpViewController.swift
@@ -8,9 +8,77 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIPageViewController {
 
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [newViewController(name: "First"), newViewController(name: "Second"), newViewController(name: "Third")]
+    }()
+    
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: name)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first{
+            setViewControllers([firstViewController], direction: .forward, animated: false, completion: nil)
+        }
+    }
+    
+    
+}
+extension HelpViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = orderedViewControllers.firstIndex(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
     }
 }
